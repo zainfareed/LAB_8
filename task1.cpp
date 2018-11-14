507868c6cd6d213a2e2bd98ba21731fb4b0e6f4f
zainfareed <zfareed2017@namal.edu.pk>
2018-11-14 21:18:16 +0500

    i have make some changes in it

--- task1.cpp
+++ task1.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
-class marks
+class Marks
 {
 private:
 int i,j,list[4][4],numbers;
 public:
-marks()
+Marks()
 {
 i=1;
 j=1;
@@ -16,17 +16,17 @@ int grade_set()
 for(i=1;i<4;i++)
  {
  for(j=1;j<4;j++)
-     { 
-      cout<<"enter the student "<<i<<" marks"<<endl;
+     {
+      cout<<"enter the student "<<i<<" marks : ";
       cin>>numbers;
       if (numbers>100 || numbers<0)
-     { 
+     {
       cout<<"enter valid marks"<<endl;
       j--;
      }
 
    else
-     {   
+     {
       list[i][j]=numbers;
      }
  }
@@ -36,7 +36,7 @@ int avg(int m)
 {
 int average=0,total=0;
  for (j=1;j<4;j++)
-  {  
+  {
       total +=list[m][j];
   }
 average=total/3;
@@ -51,18 +51,19 @@ for (i=1;i<4;i++)
 cout<<"student "<<i<<" marks:"<<endl;
   for (j=1;j<4;j++)
    {
-cout<<list[i][j]<<endl;
+cout<<" | "<<list[i][j];
    }
+   cout << '\n';
 }
 }
 };
 int main()
 {
 int ag=0;
-marks obj1;
+Marks obj1;
 obj1.grade_set();
 obj1.show_grades();
-cout<<"enter the number of student to check its average"<<endl;
+cout<<"enter the number of student to check its average : ";
 cin>>ag;
 cout<<obj1.avg(ag)<<endl;
 //return 0;
